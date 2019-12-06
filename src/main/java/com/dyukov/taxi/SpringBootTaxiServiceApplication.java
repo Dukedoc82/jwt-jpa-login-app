257a2dfa680cb9b8c0f7eac2f03282b5ac66e78c
Dmitry Dyukov <dmitry_dyukov@epam.com>
2019-12-06 17:07:17 +0300

    Implement error pages

--- src/main/java/com/dyukov/taxi/SpringBootTaxiServiceApplication.java
+++ src/main/java/com/dyukov/taxi/SpringBootTaxiServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class SpringBootTaxiServiceApplication extends SpringBootServletInitializer {
 
     @Override
--- src/main/java/com/dyukov/taxi/controller/JwtAuthenticationController.java
+++ src/main/java/com/dyukov/taxi/controller/JwtAuthenticationController.java
@@ -57,23 +57,6 @@ public class JwtAuthenticationController {
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(null);
     }
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
-            }
-        }
-        return "error";
-    }
-
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public UserDao registerUser(@RequestBody RegistrationData registrationData) {
         return userDetailsService.save(registrationData);
